ac1f877389eb81887d97f49e6c4479269cbcda1a
NowOrNever <nowornever@NowOrNeverdeMac.local>
2017-08-26 09:28:43 +0800

    first version

---
+++ Tree/Tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tree
+//
+//  Created by NowOrNever on 26/08/2017.
+//  Copyright © 2017 NowOrNever. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
